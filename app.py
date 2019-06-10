4ee6a44d1b402e327e7c74e100b67d6bd725fc3c
Shawn Loh <shawnlohb@gmail.com>
2019-06-10 17:52:06 +0800

    Added JWT claims

--- app.py
+++ app.py
@@ -16,8 +16,19 @@ app.config['JWT_SECRET_KEY'] = 'very_secret_key'
 # app.secret_key = 'very_secret_key'
 api = Api(app)
 
-
 jwt = JWTManager(app)
+
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {
+            'is_admin': True
+        }
+    return {
+        'is_admin': False
+    }
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import reqparse, Resource
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_claims
 
 from models.item import ItemModel
 
@@ -39,7 +39,14 @@ class Item(Resource):
 
         return item.json(), 201
 
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {
+                'message': 'Admin privilege required.'
+            }, 401
+
         item = ItemModel.find_by_name(name)
 
         if item:
