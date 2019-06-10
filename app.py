9f3d68be47bc96a7a0bea80a397b540027fbac6a
Shawn Loh <shawnlohb@gmail.com>
2019-06-10 18:38:29 +0800

    Added blacklisted token

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['JWT_SECRET_KEY'] = 'very_secret_key'
+app.config['JWT_SECRET_KEY'] = os.environ.get(
+    'SECRET_KEY', 'very_secret_key')
 # app.secret_key = 'very_secret_key'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 api = Api(app)
 
 jwt = JWTManager(app)
@@ -28,6 +32,51 @@ def add_claims_to_jwt(identity):
     return {
         'is_admin': False
     }
+
+
+@jwt.token_in_blacklist_loader
+def blacklisted_token_cb(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
+
+
+@jwt.expired_token_loader
+def expired_token_cb():
+    return jsonify({
+        'description': 'Token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_cb(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_cb(error):
+    return jsonify({
+        'description': 'Request does not contain an access token',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_cb():
+    return jsonify({
+        'description': 'Token is not fresh',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_cb():
+    return jsonify({
+        'description': 'Token has been revoked',
+        'error': 'token_revoked'
+    }), 401
 
 
 api.add_resource(Store, '/store/<string:name>')
