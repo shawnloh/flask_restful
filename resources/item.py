dd9a67003feeaeb3717f06002849845227c96d1e
Shawn Loh <shawnlohb@gmail.com>
2019-06-10 18:22:22 +0800

    Require fresh token to post a new item

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import reqparse, Resource
-from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity, fresh_jwt_required
 
 from models.item import ItemModel
 
@@ -21,6 +21,7 @@ class Item(Resource):
             'message': 'Item not found'
         }, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {
