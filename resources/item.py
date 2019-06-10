4cb77d9e80db02cd7edd9701be1ec99e8fb22ca4
Shawn Loh <shawnlohb@gmail.com>
2019-06-10 18:01:40 +0800

    Added JWT optional

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import reqparse, Resource
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 
 from models.item import ItemModel
 
@@ -71,9 +71,19 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+
+    @jwt_optional
     def get(self):
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {
+                'items': items
+            }
+
         return {
-            'items': [item.json() for item in ItemModel.find_all()]
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you log in'
         }
         # return {
         #     'items': list(map(lambda x: x.json(), ItemModel.query.all()))
